6aeb630e8315326155e3d83fbef1feae08fb17e7
minty <77310871+mintyfrankie@users.noreply.github.com>
2023-03-09 13:21:43 +0100

    Impose Neovim-Vscode module

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 require('basic')
 require('plugins')
 require('keybindings')
-require('colorscheme')
 require('misc')
 
--- Plugins
-require('plugin-config.nvim-treesitter')
-require('plugin-config.bufferline')
-require('plugin-config.telescope')
-require('plugin-config.nvim-tree')
-require('plugin-config.lualine')
+if not vim.g.vscode then
+    require('colorscheme')
+    require('plugin-config.nvim-treesitter')
+    require('plugin-config.bufferline')
+    require('plugin-config.telescope')
+    require('plugin-config.nvim-tree')
+    require('plugin-config.lualine')
+end
